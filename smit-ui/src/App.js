131ae9125f12c3183c04db71e95fece4743ef3c7
interfpro <interfpro@gmail.com>
2024-07-21 11:06:59 +0300

    Little fixes

--- smit-ui/src/App.js
+++ smit-ui/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Booking from './Booking';
+import Management from './Management';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Broneerimine</Link>
+            </li>
+            <li>
+              <Link to="/management">Haldus</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Booking />} />
+          <Route path="/management" element={<Management />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
